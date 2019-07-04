0861a7a21d59422f2938dbdd09f37dafa0aa5a6b
betvotenet <020mrb@gmail.com>
2019-07-04 18:39:37 -0300

    removed string lista x

--- js/Candidatos.js
+++ js/Candidatos.js
@@ -3,14 +3,14 @@ candidatos = null;
 function CargarCandidatos() {
 	candidatos = [
 	// ID_CANDIDATO_PARTIDO, NOMBRE_CANDIDATO, LISTA_CANDIDATO, slug, PARTIDO_CANDIDATO, TWITTER_CANDIDATO, address[2]ANDIDATO, BET_DEPRECATED, YOUTUBE
-	Candidato(0, 'Mauricio Macri', 'LISTA X', 'macri', 1,'@mauriciomacri', "bc1qmacrl000gactqwexqppl3rvnp2t2gd0056pzw4", "", "tqj6Cc9bwmc"), 
-	Candidato(0, 'Alberto Fernández', 'LISTA X', 'fernandez', 0, '@alferdez', "bc1qfernandex5qxk2xptn78075cuqfq2ugh008pxx", "", "XlEVkzu9BKw"),
-	Candidato(0, 'Roberto Lavagna', 'LISTA X', 'lavagna', 3, '@RLavagna', "bc1qlavagna0v8vlzzkcsc9kr97lmqtsfym6ja0nxx", "", "_Ei05g_hK9g"),
-	Candidato(0, 'José Luis Espert', 'LISTA X', 'espert', 5, '@jlespert', "bc1qespert00d9nst6qvsy5pqdwysgn0e28h2f4cqv", "", "nBN5Ksy7M4Q"),
-	Candidato(0, 'Nicolás del Caño', 'LISTA X', 'delcano', 2, '@NicolasdelCano', "bc1qdelcan00s7mkpu479qm7dlhwr5jhyktj4c5f8m", "", "WKNONOW4AVg"), 
-	Candidato(0, 'Manuela Castañeira', 'LISTA X', 'castaneira', 4, '@manuelac22', "bc1qcastanel6lzuutmjm7kmjyk4szkl72jj8uyg6v", "", "uWtBV7-TidI"),
-	Candidato(0, 'José Gómez Centurion', 'LISTA X', 'centurion', 6, '@juanjomalvinas', "bc1qcenturl0j247x6rr728kphu3f84r9h2yzze20z", "", "o1WUb13pArU"), 
-	Candidato(0, 'José A. Romero Feris', 'LISTA X', 'feris', 8, '@romeroferis', "bc1qferls00007p5drz6vq9xveypenxgjy962wsxsd", "", "t4vQBWbotVU"),
-	Candidato(0, 'Alejandro Biondini', 'LISTA X', 'biondini', 7, '@BiondiniAR', "bc1q3l0ndlnlw5kuz4c92evghms67fg2jqwul5498a", "", "oX-9RomRFY0")
+	Candidato(0, 'Mauricio Macri', '', 'macri', 1,'@mauriciomacri', "bc1qmacrl000gactqwexqppl3rvnp2t2gd0056pzw4", "", "tqj6Cc9bwmc"), 
+	Candidato(0, 'Alberto Fernández', '', 'fernandez', 0, '@alferdez', "bc1qfernandex5qxk2xptn78075cuqfq2ugh008pxx", "", "XlEVkzu9BKw"),
+	Candidato(0, 'Roberto Lavagna', '', 'lavagna', 3, '@RLavagna', "bc1qlavagna0v8vlzzkcsc9kr97lmqtsfym6ja0nxx", "", "_Ei05g_hK9g"),
+	Candidato(0, 'José Luis Espert', '', 'espert', 5, '@jlespert', "bc1qespert00d9nst6qvsy5pqdwysgn0e28h2f4cqv", "", "nBN5Ksy7M4Q"),
+	Candidato(0, 'Nicolás del Caño', '', 'delcano', 2, '@NicolasdelCano', "bc1qdelcan00s7mkpu479qm7dlhwr5jhyktj4c5f8m", "", "WKNONOW4AVg"), 
+	Candidato(0, 'Manuela Castañeira', '', 'castaneira', 4, '@manuelac22', "bc1qcastanel6lzuutmjm7kmjyk4szkl72jj8uyg6v", "", "uWtBV7-TidI"),
+	Candidato(0, 'José Gómez Centurion', '', 'centurion', 6, '@juanjomalvinas', "bc1qcenturl0j247x6rr728kphu3f84r9h2yzze20z", "", "o1WUb13pArU"), 
+	Candidato(0, 'José A. Romero Feris', '', 'feris', 8, '@romeroferis', "bc1qferls00007p5drz6vq9xveypenxgjy962wsxsd", "", "t4vQBWbotVU"),
+	Candidato(0, 'Alejandro Biondini', '', 'biondini', 7, '@BiondiniAR', "bc1q3l0ndlnlw5kuz4c92evghms67fg2jqwul5498a", "", "oX-9RomRFY0")
 	];
 }
